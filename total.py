759f050ea8b7e6bcd0610f1688d5de91df84eb47
alexmajorov <45354290+alexmajorov@users.noreply.github.com>
2019-03-24 18:03:36 +0300

    Вынес имя загружаемого файла в отдельную переменную

--- total.py
+++ total.py
@@ -2,16 +2,55 @@ from flask import Flask, url_for, session, redirect, render_template, request, j
     make_response, \
     request
 import os
+from db import DB
+from login import LoginForm
+from user import UsersModel
 
+db = DB()
 app = Flask(__name__)
 app.secret_key = 'any random string'
 UPLOAD_FOLDER = "Загрузки"
+UsersModel(db.get_connection()).init_table()
+FILE_NAME = "sample.txt"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    global i
+    i = False
+    if form.validate_on_submit():
+        user_name = form.username.data
+        password = form.password.data
+        user_model = UsersModel(db.get_connection())
+        exists = user_model.exists(user_name, password)
+        if (exists[0]):
+            session['username'] = user_name
+            session['user_id'] = exists[1]
+            if exists[1] == 4:
+                return redirect("/title_admin")
+            return redirect("/title")
+    return render_template('loginform.html', title='Авторизация', form=form)
+
+
+i = True
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('title.html', text1=open('text_init.txt').read())
+@app.route('/title')
+def title():
+    global i
+    if 'username' not in session or i:
+        return render_template('title_out.html', text1=open('text_init.txt').read())
+    return render_template('title_in.html', text1=open('text_init.txt').read(),
+                           username=session['username'])
+
+
+@app.route('/out')
+def out():
+    global i
+    i = True
+    return render_template('title_out.html', text1=open('text_init.txt').read())
 
 
 @app.route('/lab')
@@ -41,19 +80,43 @@ def staff():
 
 @app.route('/upload_file', methods=['POST', 'GET'])
 def sample_file_upload():
+    global i
+    if 'username' not in session or i:
+        return redirect('/login')
     if request.method == 'GET':
-        return render_template('upload_file.html',
-                               text=open("Описание_заказа.txt").read() + "\n")
+        return render_template('upload_file.html', text=open("Описание_заказа.txt").read() + "\n")
     elif request.method == 'POST':
         if not os.path.exists(UPLOAD_FOLDER):
             os.makedirs(UPLOAD_FOLDER)
         f = request.files['file']
+        name = request.form.get('order')
+        t = request.form.get('about')
         x = f.__repr__()
         tmp = f.read()
-        n = open(UPLOAD_FOLDER + "/" + x[x.index("'") + 1:x.index("'", 15)], "wb")
+        FILE_NAME = x[x.index("'") + 1:x.index("'", 15)]
+        n = open(UPLOAD_FOLDER + "/" + FILE_NAME, "wb")
         n.write(tmp)
         n.close()
         return "Ваш заказ ожидает обработки. <a href='/title'>Вернуться на главную</a>"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = LoginForm()
+    global i
+    i = False
+    if request.method == 'GET':
+        return render_template('loginform.html', title='Зарегистрироваться', form=form)
+    elif request.method == 'POST':
+        user_name = form.username.data
+        password = form.password.data
+        user_model = UsersModel(db.get_connection())
+        user_model.insert(user_name, password)
+        exists = user_model.exists(user_name, password)
+        if (exists[0]):
+            session['username'] = user_name
+            session['user_id'] = exists[1]
+        return redirect("/title")
 
 
 if __name__ == '__main__':
