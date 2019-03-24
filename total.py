b3482ce66ea6a03e7bc723e53035b1e754c8b564
fyodorussia <fvdelows@mail.ru>
2019-03-24 11:33:53 +0300

    залил первую версию загрузки файлов

--- total.py
+++ total.py
@@ -1,9 +1,11 @@
 from flask import Flask, url_for, session, redirect, render_template, request, jsonify, \
     make_response, \
     request
+import os
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
+UPLOAD_FOLDER = "Загрузки"
 
 
 @app.route('/')
@@ -35,6 +37,22 @@ def filament():
 @app.route('/staff')
 def staff():
     return render_template('staff.html', staff='Сайт на доработке')
+
+
+@app.route('/upload_file', methods=['POST', 'GET'])
+def sample_file_upload():
+    if request.method == 'GET':
+        return render_template('upload_file.html', filament='Сайт на доработке')
+    elif request.method == 'POST':
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        f = request.files['file']
+        x = f.__repr__()
+        tmp = f.read()
+        n = open(UPLOAD_FOLDER + "/" + x[x.index("'") + 1:x.index("'", 15)], "wb")
+        n.write(tmp)
+        n.close()
+        return "Ваш заказ ожидает обработки. <a href='/title'>Вернуться на главную</a>"
 
 
 if __name__ == '__main__':
